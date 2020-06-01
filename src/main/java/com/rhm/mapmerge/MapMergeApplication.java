d5147609f5517a872442f60fa5167a86d77750bc
Raul Herranz Munoz <raul.herranz@scmspain.com>
2020-06-01 09:59:47 +0200

    First commit

---
+++ src/main/java/com/rhm/mapmerge/MapMergeApplication.java
@@ -0,0 +1,13 @@
+package com.rhm.mapmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapMergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhm/mapmerge/MapMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhm.mapmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
